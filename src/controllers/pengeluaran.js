a50aeec8c243a8b3ea7badd91a92de560dc7bbe7
simcle <nendisofiandy84@gmail.com>
2024-05-23 11:14:23 +0700

    udpate pengeluaran admin

--- src/controllers/pengeluaran.js
+++ src/controllers/pengeluaran.js
@@ -31,7 +31,36 @@ exports.insertPengeluaran = (req, res) => {
         res.status(200).json(result)
     })
 }
-
+exports.getPengeluaranAdmin = (req, res) => {
+    const shopId = '647aa84733581aaca9c7725b'
+    const date = new Date();
+    let today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+    PengeluaranModel.find({$and: [{shopId: shopId}, {createdAt: {$gte: today}}]}).sort({createdAt: -1})
+    .then(result => {
+        res.status(200).json(result)
+    })
+}
+exports.insertPengeluaranAdmin = (req, res) => {
+    const shopId = '647aa84733581aaca9c7725b'
+    const userId = req.user._id
+    const date = new Date();
+    let today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+    const pengeluaran = new PengeluaranModel({
+        shopId: shopId,
+        item: req.body.item,
+        harga: req.body.harga,
+        qty: req.body.qty,
+        total: req.body.total,
+        userId: userId
+    })
+    pengeluaran.save()
+    .then(() => {
+        return PengeluaranModel.find({$and: [{shopId: shopId}, {createdAt: {$gte: today}}]}).sort({createdAt: -1})
+    })
+    .then(result => {
+        res.status(200).json(result)
+    })
+}
 exports.deletePengeluaran = (req, res) => {
     const id = req.params.pengeluaranId
     const shopId = req.user.shopId
--- src/routers/pengeluaran.js
+++ src/routers/pengeluaran.js
@@ -5,6 +5,8 @@ const pengeluaranController = require('../controllers/pengeluaran');
 
 router.get('/', pengeluaranController.getPengeluaran);
 router.post('/', pengeluaranController.insertPengeluaran);
+router.get('/admin', pengeluaranController.getPengeluaranAdmin);
+router.post('/admin', pengeluaranController.insertPengeluaranAdmin);
 router.delete('/:pengeluaranId', pengeluaranController.deletePengeluaran);
 
 module.exports = router;
